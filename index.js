806a36e3b4f96ccb73a9dcbaf3e430b591ddb3ee
romdotdog <romdotdog@users.noreply.github.com>
2021-05-23 03:07:23 -0500

    Switch from debug build to optimized build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const loader = require("@assemblyscript/loader");
 
 const wasmModule = loader.instantiateSync(
-	fs.readFileSync(__dirname + "/build/untouched.wasm")
+	fs.readFileSync(__dirname + "/build/optimized.wasm")
 );
 
 const { encode, decode, __newString, __getString } = wasmModule.exports;
